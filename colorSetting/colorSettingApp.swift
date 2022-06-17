023b47acd1609bca33f91a4808348123964d7ba8
Denis <varenikovda@gmail.com>
2022-06-17 18:05:03 +0300

    Initial Commit

---
+++ colorSetting/colorSettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorSettingApp.swift
+//  colorSetting
+//
+//  Created by Денис on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct colorSettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
